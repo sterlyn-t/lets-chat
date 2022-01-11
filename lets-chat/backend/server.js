247689b2c60da15d6aadaffd4567bcd3d5618262
sterlyn-t <sterlyntang31@gmail.com>
2022-01-11 13:43:01 -0500

    US #7 Incorporated Socket.IO for messaging

--- lets-chat/backend/server.js
+++ lets-chat/backend/server.js
@@ -36,4 +36,48 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+// Creating connection
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    // Creating a new room
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User joined room: " + room);
+    });
+
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users || chat.users === null || chat.users === undefined) return console.log("chat.users not defined");
+
+        chat.users.forEach(user => {
+            if (user._id === newMessageReceived.sender._id){
+                return;
+            }
+            socket.in(user._id).emit("message received", newMessageReceived);
+        })
+    });
+
+    socket.on('typing', (room) => socket.in(room).emit("typing"));
+    socket.on('stop typing', (room) => socket.in(room).emit("stop typing"));
+
+    socket.off("setup", () => {
+        console.log("User Disconnected");
+        socket.leave(userData._id);
+    })
+
+});
--- lets-chat/frontend/src/components/SingleChat.js
+++ lets-chat/frontend/src/components/SingleChat.js
@@ -11,7 +11,12 @@ import axios from 'axios';
 import { FormControl } from '@chakra-ui/form-control';
 import './styles.css';
 import ScrollableChat from './ScrollableChat';
+import io from 'socket.io-client';
+import Lottie from 'react-lottie';
+import animationData from '../animations/chat-typing-indicator.json';
 
+const ENDPOINT = "http://localhost:8080";
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
@@ -19,8 +24,20 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     const [loading, setLoading] = useState(false);
     const [newMessage, setNewMessage] = useState();
     const { user, selectedChat, setSelectedChat } = ChatState();
+    const [socketConnected, setSocketConnected] = useState(false);
+    const [typing, setTyping] = useState(false);
+    const [isTyping, setIsTyping] = useState(false);
 
     const toast = useToast();
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    }
 
     const fetchMessages = async () => {
         if (!selectedChat) return;
@@ -36,6 +53,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             console.log(messages);
             setMessages(data);
             setLoading(false);
+            socket.emit("join chat", selectedChat._id);
         } catch (error) {
             toast({
                 title: "Error Occured!",
@@ -50,11 +68,30 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
     useEffect(() => {
         fetchMessages();
+        selectedChatCompare = selectedChat;
     }, [selectedChat]);
+
+    useEffect(() => {
+        socket = io(ENDPOINT);
+        socket.emit("setup", user);
+        socket.on("connected", () => setSocketConnected(true));
+        socket.on("typing", () => setIsTyping(true));
+        socket.on("stop typing", () => setIsTyping(false));
+      }, [])
+
+    useEffect(() => {
+        socket.on("message received", (newMessageReceived) => {
+            if (!selectedChatCompare || selectedChatCompare._id !== newMessageReceived.chat._id){
+                // notification
+            } else {
+                setMessages([...messages, newMessageReceived]);
+            }
+        })
+    })
 
     const sendMessage = async (event) => {
         if (event.key === "Enter" && newMessage) {
-         // socket.emit("stop typing", selectedChat._id);
+          socket.emit("stop typing", selectedChat._id);
           try {
             const config = {
               headers: {
@@ -71,7 +108,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
               },
               config
             );
-           // socket.emit("new message", data);
+            socket.emit("new message", data);
             setMessages([...messages, data]);
           } catch (error) {
               console.log("frontend error")
@@ -86,8 +123,30 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           }
         }
       };
+
     const typingHandler = (e) => {
         setNewMessage(e.target.value);
+
+        // Typing indicator logic
+        if (!socketConnected) return;
+
+        if (!typing){
+            setTyping(true);
+            socket.emit("typing", selectedChat._id);
+        }
+
+        // When to stop typing
+        let lastTypingTime = new Date().getTime();
+        var timerLength = 3000;
+        setTimeout(() => {
+            var timeNow = new Date().getTime();
+            var timeDifference = timeNow - lastTypingTime;
+
+            if (timeDifference >= timerLength && typing){
+                socket.emit("stop typing", selectedChat._id);
+                setTyping(false);
+            }
+        }, timerLength)
     }
 
     return (
@@ -150,6 +209,13 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                )}
 
                <FormControl onKeyDown={sendMessage}  isRequired mt={3}>
+                {isTyping ? <div>
+                    <Lottie 
+                    options={defaultOptions}
+                    width={70}
+                    style={{ marginBottom: 15, marginLeft: 0}}
+                    />
+                </div> : (<></>)}
                 <Input 
                     variant="filled"
                     bg="#E0E0E0"
